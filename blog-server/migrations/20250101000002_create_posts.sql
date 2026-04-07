01fde36dca473c9e5157350e0c7f7b7a096da251
Sergey Rusakov <s.rusakov@kefirgames.com>
2026-04-07 20:37:07 +0300

    add: migrations

---
+++ blog-server/migrations/20250101000002_create_posts.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE IF NOT EXISTS posts (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    author_id UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_post_author ON posts(author_id);
+CREATE INDEX IF NOT EXISTS idx_post_created_date ON posts(created_at);

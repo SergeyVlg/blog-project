658907b4d4b7624ef51691320fc4d2c0437dd077
Sergey Rusakov <s.rusakov@kefirgames.com>
2026-04-09 21:15:56 +0300

    add: grpc service draft

---
+++ blog-server/src/presentation/grpc_service.rs
@@ -0,0 +1,36 @@
+﻿use tonic::{Request, Response, Status};
+use crate::presentation::proto::blog_service_server::BlogService as GrpcBlogService;
+use crate::presentation::proto::{CreatePostRequest, CreatePostResponse, DeletePostRequest, DeletePostResponse, GetPostRequest, GetPostResponse, ListPostsRequest, ListPostsResponse, LoginRequest, LoginResponse, RegisterRequest, RegisterResponse, UpdatePostRequest, UpdatePostResponse};
+
+struct BlogGrpcService;
+
+#[tonic::async_trait]
+impl GrpcBlogService for BlogGrpcService {
+    async fn register(&self, request: Request<RegisterRequest>) -> Result<Response<RegisterResponse>, Status> {
+        todo!()
+    }
+
+    async fn login(&self, request: Request<LoginRequest>) -> Result<Response<LoginResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_post(&self, request: Request<CreatePostRequest>) -> Result<Response<CreatePostResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_post(&self, request: Request<GetPostRequest>) -> Result<Response<GetPostResponse>, Status> {
+        todo!()
+    }
+
+    async fn update_post(&self, request: Request<UpdatePostRequest>) -> Result<Response<UpdatePostResponse>, Status> {
+        todo!()
+    }
+
+    async fn delete_post(&self, request: Request<DeletePostRequest>) -> Result<Response<DeletePostResponse>, Status> {
+        todo!()
+    }
+
+    async fn list_posts(&self, request: Request<ListPostsRequest>) -> Result<Response<ListPostsResponse>, Status> {
+        todo!()
+    }
+}
--- blog-server/src/presentation/mod.rs
+++ blog-server/src/presentation/mod.rs
@@ -2,3 +2,5 @@ pub(crate) mod http_handlers;
 mod dto;
 mod auth;
 pub(crate) mod middleware;
+mod grpc_service;
+pub(crate) mod proto;

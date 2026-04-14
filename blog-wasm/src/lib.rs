6cd7628294877b9f7c1fc5235642af4b5b54d656
Sergey Rusakov <s.rusakov@kefirgames.com>
2026-04-14 21:27:35 +0300

    add: wasm draft

--- blog-wasm/src/lib.rs
+++ blog-wasm/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use dioxus::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[component]
+pub fn App() -> Element {
+    rsx! {
+        main {
+            "Hello world"
+        }
     }
 }
---
+++ blog-wasm/src/main.rs
@@ -0,0 +1,13 @@
+﻿#[cfg(target_arch = "wasm32")]
+use blog_wasm::App;
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    dioxus::launch(App);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    println!("blog-wasm is intended to run in the browser via the wasm32-unknown-unknown target.");
+}
+
